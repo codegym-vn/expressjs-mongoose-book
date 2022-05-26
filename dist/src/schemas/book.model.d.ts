79c2ba09592c75d57ae1ed7f81404ac5fbd6ece6
hungnv0902 <hungnv0902@gmail.com>
2022-05-26 22:12:18 +0700

    first commit

---
+++ dist/src/schemas/book.model.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="mongoose/types/aggregate" />
+/// <reference types="mongoose/types/connection" />
+/// <reference types="mongoose/types/cursor" />
+/// <reference types="mongoose/types/document" />
+/// <reference types="mongoose/types/error" />
+/// <reference types="mongoose/types/mongooseoptions" />
+/// <reference types="mongoose/types/pipelinestage" />
+/// <reference types="mongoose/types/schemaoptions" />
+/// <reference types="mongoose" />
+interface IBook {
+    title: string;
+    description: string;
+    author: string;
+}
+declare const Book: import("mongoose").Model<IBook, {}, {}, {}>;
+export { Book };
